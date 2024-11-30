8e99a1ec033ca73e44d4c395388d01160df03e98
hyunnjjung <hyunnjjung@gmail.com>
2024-12-01 05:58:38 +0900

    [Feat] Django로 chat일단 구현

--- PartyLink/PartyLink/asgi.py
+++ PartyLink/PartyLink/asgi.py
@@ -3,14 +3,19 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from room.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PartyLink.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
--- PartyLink/chat/urls.py
+++ PartyLink/chat/urls.py
@@ -1,8 +1,9 @@
+# chat/urls.py
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('rooms/', views.ChatRoomListCreateView.as_view(), name='chat_rooms'),
-    path('<int:room_id>/messages', views.MessageListView.as_view(), name='chat_messages'),
+    path("", views.index, name="index"),
+    path("<str:room_name>/", views.room, name="room"),
 ]
