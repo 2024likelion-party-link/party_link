3d2626e753f94da3ee8e2247dd07d72915d9a976
Jiye-Park01 <jiye0710@gmail.com>
2024-12-01 10:37:23 +0900

    [chore]실시간 채팅 확인 완료 최종 끝

--- PartyLink/PartyLink/asgi.py
+++ PartyLink/PartyLink/asgi.py
@@ -1,16 +1,50 @@
-import os
-from django.core.asgi import get_asgi_application
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from room.routing import websocket_urlpatterns
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PartyLink.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             websocket_urlpatterns
+#         )
+#     ),
+# })
+
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from .routing import websocket_urlpatterns
+# from django.core.asgi import get_asgi_application
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(
+#             URLRouter(websocket_urlpatterns)
+#         )
+#     ),
+# })
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from room.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PartyLink.settings')
+from channels.security.websocket import AllowedHostsOriginValidator
+from room.routing import websocket_urlpatterns as room_websocket
+from chat.routing import websocket_urlpatterns as chat_websocket
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                room_websocket + chat_websocket
+            )
         )
     ),
 })
