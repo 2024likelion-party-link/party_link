07d347c46afc70e74e7cf25998efc7ecda128f26
Jiye-Park01 <jiye0710@gmail.com>
2024-12-01 11:28:07 +0900

    [add]handgame -ing

--- PartyLink/handGame/models.py
+++ PartyLink/handGame/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
+from room.models import Room
 
-# Create your models here.
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    sender = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["timestamp"]
